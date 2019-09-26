3dee787e1ad5a7b6d5f1d717f5a2be62f8248b36
Regan Sarwas <Regan_Sarwas@nps.gov>
2019-09-26 13:07:20 -0800

    Lint to StandardJS

--- csv2geo.js
+++ csv2geo.js
@@ -4,170 +4,189 @@
  * 2) https://github.com/d3/d3-dsv/blob/master/src/dsv.js
  */
 
+/* global L */
+
 L.GeoCSV = L.GeoJSON.extend({
 
   options: {
     firstLineTitles: true,
     fieldSeparator: ',',
-	  latitudeTitle: 'latitude', //Case sensitive, no mangling or space removal
-	  longitudeTitle: 'longitude', //ditto
-    titles: ['lat', 'lng', 'popup'], //ignored if firstLineTitles = true
+    latitudeTitle: 'latitude', // Case sensitive, no mangling or space removal
+    longitudeTitle: 'longitude', // ditto
+    titles: ['lat', 'lng', 'popup'] // ignored if firstLineTitles = true
   },
 
   initialize: function (csv, options) {
-    L.Util.setOptions (this, options);
-    L.GeoJSON.prototype.initialize.call (this, csv, options);
+    L.Util.setOptions(this, options)
+    L.GeoJSON.prototype.initialize.call(this, csv, options)
   },
 
   addData: function (data) {
     if (typeof data === 'string') {
-      var csv = this._dsv(this.options.fieldSeparator);
-      var rows;
+      var csv = this._dsv(this.options.fieldSeparator)
+      var rows
       if (this.options.firstLineTitles) {
-        rows = csv.parse(data);
+        rows = csv.parse(data)
         // rows will be an array of objects
-        this.options.titles = rows.columns;
+        this.options.titles = rows.columns
       } else {
-        rows = csv.parseRows(data);
-        //rows will be an array of arrays
+        rows = csv.parseRows(data)
+        // rows will be an array of arrays
       }
-      //convertimos los datos a geoJSON
-      data = this._csv2json(rows);
+      data = this._csv2json(rows)
     }
-		return L.GeoJSON.prototype.addData.call (this, data);
+    return L.GeoJSON.prototype.addData.call(this, data)
   },
 
   _csv2json: function (rows) {
-    function pointFeature(lat, lon, props) {
+    function pointFeature (lat, lon, props) {
       return {
-        type: "Feature",
+        type: 'Feature',
         geometry: {
-          type: "Point",
-          coordinates: [lon,lat]
+          type: 'Point',
+          coordinates: [lon, lat]
         },
         properties: props
-      };
+      }
     }
     var json = {
-      type: "FeatureCollection"
-    };
+      type: 'FeatureCollection'
+    }
     if (this.options.firstLineTitles) {
-      json["features"] = rows.map(element => {
-        var lat = element[this.options.latitudeTitle];
-        var lon = element[this.options.longitudeTitle];
+      json.features = rows.map(element => {
+        var lat = element[this.options.latitudeTitle]
+        var lon = element[this.options.longitudeTitle]
         // TODO return null if lat/lon is invalid
-        props = Object.assign({}, element);
-        delete props[this.options.latitudeTitle];
-        delete props[this.options.longitudeTitle];
-        return pointFeature(lat,lon,props);
-      });
+        var props = Object.assign({}, element)
+        delete props[this.options.latitudeTitle]
+        delete props[this.options.longitudeTitle]
+        return pointFeature(lat, lon, props)
+      })
     } else {
-      var ilat = this.options.titles.indexOf(this.options.latitudeTitle);
-      var ilon = this.options.titles.indexOf(this.options.longitudeTitle);
-      json["features"] = rows.map(row => {
-        var lat = row[ilat];
-        var lon = row[ilon];
+      var ilat = this.options.titles.indexOf(this.options.latitudeTitle)
+      var ilon = this.options.titles.indexOf(this.options.longitudeTitle)
+      json.features = rows.map(row => {
+        var lat = row[ilat]
+        var lon = row[ilon]
         // TODO return null if lat/lon is invalid
-        props = {}
+        var props = {}
         row.forEach((element, i) => {
-          if (i != ilat || i != ilon) {
+          if (i !== ilat || i !== ilon) {
             props[this.options.titles[i]] = element
           }
-        });
-        return pointFeature(lat,lon,props);
-      });
+        })
+        return pointFeature(lat, lon, props)
+      })
     }
-    return json;
+    return json
   },
 
-  _dsv: function(delimiter) {
-    var reFormat = new RegExp("[\"" + delimiter + "\n\r]"),
-        DELIMITER = delimiter.charCodeAt(0),
-        EOL = {},
-        EOF = {},
-        QUOTE = 34,
-        NEWLINE = 10,
-        RETURN = 13;
+  _dsv: function (delimiter) {
+    var DELIMITER = delimiter.charCodeAt(0)
+    var EOL = {}
+    var EOF = {}
+    var QUOTE = 34
+    var NEWLINE = 10
+    var RETURN = 13
 
-    function _objectConverter(columns) {
-      return new Function("d", "return {" + columns.map(function(name, i) {
-        return JSON.stringify(name) + ": d[" + i + "] || \"\"";
-      }).join(",") + "}");
+    function _objectConverter (columns) {
+      /* eslint-disable no-new-func */
+      return new Function('d', 'return {' + columns.map(function (name, i) {
+        return JSON.stringify(name) + ': d[' + i + '] || ""'
+      }).join(',') + '}')
+      /* eslint-enable no-new-func */
     }
 
-    function _customConverter(columns, f) {
-      var object = _objectConverter(columns);
-      return function(row, i) {
-        return f(object(row), i, columns);
-      };
+    function _customConverter (columns, f) {
+      var object = _objectConverter(columns)
+      return function (row, i) {
+        return f(object(row), i, columns)
+      }
     }
 
-    function parse(text, f) {
-      var convert, columns, rows = parseRows(text, function(row, i) {
-        if (convert) return convert(row, i - 1);
-        columns = row, convert = f ? _customConverter(row, f) : _objectConverter(row);
-      });
-      rows.columns = columns || [];
-      return rows;
+    function parse (text, f) {
+      var convert
+      var columns
+      var rows = parseRows(text, function (row, i) {
+        if (convert) return convert(row, i - 1)
+        columns = row
+        convert = f ? _customConverter(row, f) : _objectConverter(row)
+      })
+      rows.columns = columns || []
+      return rows
     }
 
-    function parseRows(text, f) {
-      var rows = [], // output rows
-          N = text.length,
-          I = 0, // current character index
-          n = 0, // current line number
-          t, // current token
-          eof = N <= 0, // current token followed by EOF?
-          eol = false; // current token followed by EOL?
+    function parseRows (text, f) {
+      var rows = [] // output rows
+      var N = text.length
+      var I = 0 // current character index
+      var n = 0 // current line number
+      var t // current token
+      var eof = N <= 0 // current token followed by EOF?
+      var eol = false // current token followed by EOL?
 
       // Strip the trailing newline.
-      if (text.charCodeAt(N - 1) === NEWLINE) --N;
-      if (text.charCodeAt(N - 1) === RETURN) --N;
+      if (text.charCodeAt(N - 1) === NEWLINE) --N
+      if (text.charCodeAt(N - 1) === RETURN) --N
 
-      function token() {
-        if (eof) return EOF;
-        if (eol) return eol = false, EOL;
+      function token () {
+        if (eof) return EOF
+        if (eol) {
+          eol = false
+          return EOL
+        }
 
         // Unescape quotes.
-        var i, j = I, c;
+        var i
+        var j = I
+        var c
         if (text.charCodeAt(j) === QUOTE) {
-          while (I++ < N && text.charCodeAt(I) !== QUOTE || text.charCodeAt(++I) === QUOTE);
-          if ((i = I) >= N) eof = true;
-          else if ((c = text.charCodeAt(I++)) === NEWLINE) eol = true;
-          else if (c === RETURN) { eol = true; if (text.charCodeAt(I) === NEWLINE) ++I; }
-          return text.slice(j + 1, i - 1).replace(/""/g, "\"");
+          while ((I++ < N && text.charCodeAt(I) !== QUOTE) || text.charCodeAt(++I) === QUOTE) /* empty */ ;
+          if ((i = I) >= N) eof = true
+          else if ((c = text.charCodeAt(I++)) === NEWLINE) eol = true
+          else if (c === RETURN) {
+            eol = true
+            if (text.charCodeAt(I) === NEWLINE) ++I
+          }
+          return text.slice(j + 1, i - 1).replace(/""/g, '"')
         }
 
         // Find next delimiter or newline.
         while (I < N) {
-          if ((c = text.charCodeAt(i = I++)) === NEWLINE) eol = true;
-          else if (c === RETURN) { eol = true; if (text.charCodeAt(I) === NEWLINE) ++I; }
-          else if (c !== DELIMITER) continue;
-          return text.slice(j, i);
+          if ((c = text.charCodeAt(i = I++)) === NEWLINE) eol = true
+          else if (c === RETURN) {
+            eol = true
+            if (text.charCodeAt(I) === NEWLINE) ++I
+          } else if (c !== DELIMITER) continue
+          return text.slice(j, i)
         }
 
         // Return last token before EOF.
-        return eof = true, text.slice(j, N);
+        eof = true
+        return text.slice(j, N)
       }
 
       while ((t = token()) !== EOF) {
-        var row = [];
-        while (t !== EOL && t !== EOF) row.push(t), t = token();
-        if (f && (row = f(row, n++)) == null) continue;
-        rows.push(row);
+        var row = []
+        while (t !== EOL && t !== EOF) {
+          row.push(t)
+          t = token()
+        }
+        if (f && (row = f(row, n++)) == null) continue
+        rows.push(row)
       }
 
-      return rows;
+      return rows
     }
 
     return {
       parse: parse,
-      parseRows: parseRows,
-    };
+      parseRows: parseRows
+    }
   }
 
-});
+})
 
-L.geoCsv = function (csv_string, options) {
-  return new L.GeoCSV (csv_string, options);
-};
+L.geoCsv = function (csvString, options) {
+  return new L.GeoCSV(csvString, options)
+}
--- dsv.js
+++ dsv.js
@@ -1,102 +1,141 @@
 // RFC 4180 CSV Parser
 // Extracted from https://github.com/d3/d3-dsv/blob/master/src/dsv.js
 
-var dsv = function(delimiter) {
-  var reFormat = new RegExp("[\"" + delimiter + "\n\r]"),
-      DELIMITER = delimiter.charCodeAt(0),
-      EOL = {},
-      EOF = {},
-      QUOTE = 34,
-      NEWLINE = 10,
-      RETURN = 13;
+var dsv = function (delimiter) {
+  var DELIMITER = delimiter.charCodeAt(0)
+  var EOL = {}
+  var EOF = {}
+  var QUOTE = 34
+  var NEWLINE = 10
+  var RETURN = 13
 
-  function _objectConverter(columns) {
-    return new Function("d", "return {" + columns.map(function(name, i) {
-      return JSON.stringify(name) + ": d[" + i + "] || \"\"";
-    }).join(",") + "}");
+  function _objectConverter (columns) {
+    /* eslint-disable no-new-func */
+    return new Function('d', 'return {' + columns.map(function (name, i) {
+      return JSON.stringify(name) + ': d[' + i + '] || ""'
+    }).join(',') + '}')
+    /* eslint-enable no-new-func */
   }
 
-  function _customConverter(columns, f) {
-    var object = _objectConverter(columns);
-    return function(row, i) {
-      return f(object(row), i, columns);
-    };
+  function _customConverter (columns, f) {
+    var object = _objectConverter(columns)
+    return function (row, i) {
+      return f(object(row), i, columns)
+    }
   }
 
-  function parse(text, f) {
-    var convert, columns, rows = parseRows(text, function(row, i) {
-      if (convert) return convert(row, i - 1);
-      columns = row, convert = f ? _customConverter(row, f) : _objectConverter(row);
-    });
-    rows.columns = columns || [];
-    return rows;
+  function parse (text, f) {
+    var convert
+    var columns
+    var rows = parseRows(text, function (row, i) {
+      if (convert) return convert(row, i - 1)
+      columns = row
+      convert = f ? _customConverter(row, f) : _objectConverter(row)
+    })
+    rows.columns = columns || []
+    return rows
   }
 
-  function parseRows(text, f) {
-    var rows = [], // output rows
-        N = text.length,
-        I = 0, // current character index
-        n = 0, // current line number
-        t, // current token
-        eof = N <= 0, // current token followed by EOF?
-        eol = false; // current token followed by EOL?
+  function parseRows (text, f) {
+    var rows = [] // output rows
+    var N = text.length
+    var I = 0 // current character index
+    var n = 0 // current line number
+    var t // current token
+    var eof = N <= 0 // current token followed by EOF?
+    var eol = false // current token followed by EOL?
 
     // Strip the trailing newline.
-    if (text.charCodeAt(N - 1) === NEWLINE) --N;
-    if (text.charCodeAt(N - 1) === RETURN) --N;
+    if (text.charCodeAt(N - 1) === NEWLINE) --N
+    if (text.charCodeAt(N - 1) === RETURN) --N
 
-    function token() {
-      if (eof) return EOF;
-      if (eol) return eol = false, EOL;
+    function token () {
+      if (eof) return EOF
+      if (eol) {
+        eol = false
+        return EOL
+      }
 
       // Unescape quotes.
-      var i, j = I, c;
+      var i
+      var j = I
+      var c
       if (text.charCodeAt(j) === QUOTE) {
-        while (I++ < N && text.charCodeAt(I) !== QUOTE || text.charCodeAt(++I) === QUOTE);
-        if ((i = I) >= N) eof = true;
-        else if ((c = text.charCodeAt(I++)) === NEWLINE) eol = true;
-        else if (c === RETURN) { eol = true; if (text.charCodeAt(I) === NEWLINE) ++I; }
-        return text.slice(j + 1, i - 1).replace(/""/g, "\"");
+        while ((I++ < N && text.charCodeAt(I) !== QUOTE) || text.charCodeAt(++I) === QUOTE) /* empty */ ;
+        if ((i = I) >= N) eof = true
+        else if ((c = text.charCodeAt(I++)) === NEWLINE) eol = true
+        else if (c === RETURN) {
+          eol = true
+          if (text.charCodeAt(I) === NEWLINE) ++I
+        }
+        return text.slice(j + 1, i - 1).replace(/""/g, '"')
       }
 
       // Find next delimiter or newline.
       while (I < N) {
-        if ((c = text.charCodeAt(i = I++)) === NEWLINE) eol = true;
-        else if (c === RETURN) { eol = true; if (text.charCodeAt(I) === NEWLINE) ++I; }
-        else if (c !== DELIMITER) continue;
-        return text.slice(j, i);
+        if ((c = text.charCodeAt(i = I++)) === NEWLINE) eol = true
+        else if (c === RETURN) {
+          eol = true
+          if (text.charCodeAt(I) === NEWLINE) ++I
+        } else if (c !== DELIMITER) continue
+        return text.slice(j, i)
       }
 
       // Return last token before EOF.
-      return eof = true, text.slice(j, N);
+      eof = true
+      return text.slice(j, N)
     }
 
     while ((t = token()) !== EOF) {
-      var row = [];
-      while (t !== EOL && t !== EOF) row.push(t), t = token();
-      if (f && (row = f(row, n++)) == null) continue;
-      rows.push(row);
+      var row = []
+      while (t !== EOL && t !== EOF) {
+        row.push(t)
+        t = token()
+      }
+      if (f && (row = f(row, n++)) == null) continue
+      rows.push(row)
     }
 
-    return rows;
+    return rows
   }
 
   return {
     parse: parse,
-    parseRows: parseRows,
-  };
+    parseRows: parseRows
+  }
 }
 
-var csv = dsv(",");
-
-//var text = 'foo,bar\n1,2\na,"b,c"\nregan,"is ""great""!"\n2019-10-01,12:23:45'
+// Test 1
+/*
+var csv = dsv(',')
+var text = 'foo,bar\n1,2'
+var r1 = csv.parse(text)
+console.log(r1)
+// expect: [ { foo: '1', bar: '2' }, columns: [ 'foo', 'bar' ] ]
+console.log(r1.columns)
+// expect: [ 'foo', 'bar' ]
+console.log(r1.length)
+// expect: 1
+console.log(r1[0].bar)
+// expect: 2
+var r2 = csv.parseRows(text)
+console.log(r2)
+// expect: [ [ 'foo', 'bar' ], [ '1', '2' ] ]
+console.log(r2.columns)
+// expect: undefined
+console.log(r2.length)
+// expect: 2
+console.log(r2[0].bar)
+// expect: undefined
+console.log(r2[1][1])
+// expect: 2
+*/
+// Test 2
+var text = 'foo,bar\n1,2\na,"b,c"\nregan,"is\n""great""!"\n2019-10-01,12:23:45'
+console.log(dsv(',').parse(text))
+// expect: [ { foo: '1', bar: '2' }, { foo: 'a', bar: 'b,c' }, { foo: 'regan', bar: 'is "great"!' }, { foo: '2019-10-01', bar: '12:23:45' }, columns: [ 'foo', 'bar' ]]
+// Test 3
+/*
 var text = 'City+Na.me ,lat,lon\nLos Angeles,34°03′N,118°15′W\nNew York City,40°42′46″N,74°00′21″W\nParis,48°51′24″N,2°21′03″E'
-//var text = 'foo,bar\n1,2'
-var j = csv.parse(text) // when the first row is a header row
-console.log(j) // [ { foo: '1', bar: '2' }, columns: [ 'foo', 'bar' ] ]
-//console.log(j.columns)  // 3
-//console.log(j.length)  // [ { foo: '1', bar: '2' }, columns: [ 'foo', 'bar' ] ]
-//console.log(j[0].City)  // [ { foo: '1', bar: '2' }, columns: [ 'foo', 'bar' ] ]
-//console.log(j[0][])
-//var j = csv.parseRows(text) // when the first row is data
-//console.log(j)  // [ [ 'foo', 'bar' ], [ '1', '2' ] ]
+console.log(dsv(',').parse(text))
+*/
