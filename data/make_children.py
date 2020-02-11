f4aab5e854b1dfbe927cb9bcd6bc241e7e90ca36
Regan Sarwas <regan_sarwas@nps.gov>
2020-02-11 09:35:40 -0900

    Linting

--- data/make_children.py
+++ data/make_children.py
@@ -7,40 +7,46 @@ import csv
 import collections
 import json
 
-with open('facilities.csv', 'r') as csv_fh:
-    csv_fh.readline()  # remove header
-    reader = csv.reader(csv_fh)
-    gis_loc_counts = collections.Counter([row[1] for row in reader])
+def main():
+    """Create JSON files for web app"""
 
-with open('assets.csv', 'r') as csv_fh:
-    csv_fh.readline()  # remove header
-    reader = csv.reader(csv_fh)
-    gis_asset_counts = collections.Counter([row[1] for row in reader])
+    with open('facilities.csv', 'r') as csv_fh:
+        csv_fh.readline()  # remove header
+        reader = csv.reader(csv_fh)
+        gis_loc_counts = collections.Counter([row[1] for row in reader])
 
-children = {}
-with open('parents.csv', 'r') as csv_fh:
-    csv_fh.readline()  # remove header
-    reader = csv.reader(csv_fh)
-    for row in [r for r in reader if r[0] in gis_loc_counts]:
-        parent = row[0]
-        child = {'i':row[1], 'd':row[2], 'c':gis_loc_counts[row[1]]}
-        if parent not in children:
-            children[parent] = []
-        children[parent].append(child)
+    with open('assets.csv', 'r') as csv_fh:
+        csv_fh.readline()  # remove header
+        reader = csv.reader(csv_fh)
+        gis_asset_counts = collections.Counter([row[1] for row in reader])
 
-with open('children.json', 'w') as json_fh:
-    json_fh.write(json.dumps(children, sort_keys=True, indent=2, separators=(',', ':')))
+    children = {}
+    with open('parents.csv', 'r') as csv_fh:
+        csv_fh.readline()  # remove header
+        reader = csv.reader(csv_fh)
+        for row in [r for r in reader if r[0] in gis_loc_counts]:
+            parent = row[0]
+            child = {'i':row[1], 'd':row[2], 'c':gis_loc_counts[row[1]]}
+            if parent not in children:
+                children[parent] = []
+            children[parent].append(child)
 
-assets = {}
-with open('all_assets.csv', 'r') as csv_fh:
-    csv_fh.readline()  # remove header
-    reader = csv.reader(csv_fh)
-    for row in [r for r in reader if r[0] in gis_loc_counts]:
-        parent = row[0]
-        child = {'i':row[1], 'd':row[2], 'c':gis_asset_counts[row[1]]}
-        if parent not in assets:
-            assets[parent] = []
-        assets[parent].append(child)
+    with open('children.json', 'w') as json_fh:
+        json_fh.write(json.dumps(children, sort_keys=True, indent=2, separators=(',', ':')))
 
-with open('assets.json', 'w') as json_fh:
-    json_fh.write(json.dumps(assets, sort_keys=True, indent=2, separators=(',', ':')))
+    assets = {}
+    with open('all_assets.csv', 'r') as csv_fh:
+        csv_fh.readline()  # remove header
+        reader = csv.reader(csv_fh)
+        for row in [r for r in reader if r[0] in gis_loc_counts]:
+            parent = row[0]
+            child = {'i':row[1], 'd':row[2], 'c':gis_asset_counts[row[1]]}
+            if parent not in assets:
+                assets[parent] = []
+            assets[parent].append(child)
+
+    with open('assets.json', 'w') as json_fh:
+        json_fh.write(json.dumps(assets, sort_keys=True, indent=2, separators=(',', ':')))
+
+if __name__ == '__main__':
+    main()
